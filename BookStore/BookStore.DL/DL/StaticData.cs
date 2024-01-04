db2bd4571a85a47058e10ca9170cd4626ad82465
SadikaHasan <sadikahasan17@gmail.com>
2024-01-04 19:10:08 +0200

    asvxgdscv

--- BookStore/BookStore.DL/DL/StaticData.cs
+++ BookStore/BookStore.DL/DL/StaticData.cs
@@ -11,18 +11,30 @@ namespace BookStore.DL.DL
             {
                 Id = 1,
                 Title = "1 Book",
+                AuthorId=1,
+                ReleaseDate=new DateTime(year:2005, month:05, day:07)
             },
             new Book()
             {
                 Id = 2,
                 Title = "2 Book",
+                AuthorId=1,
+                ReleaseDate=new DateTime(year:2007, month:06, day:17)
             },
             new Book()
             {
                 Id = 3,
                 Title = "3 Book",
+                AuthorId=1,
+                ReleaseDate=new DateTime(year:2009, month:10, day:27)
             },
-
+            new Book()
+            {
+                Id = 4,
+                Title = "4 Book",
+                AuthorId=1,
+                ReleaseDate=new DateTime(year:2015, month:01, day:07)
+            },
         };
         public static List<Author> Authors = new List<Author>()
         {
